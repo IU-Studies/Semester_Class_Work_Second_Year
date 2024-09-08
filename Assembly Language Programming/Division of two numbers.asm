3ec13da05ead50c591614b5f21e687c4855eba1d
IU-Studies <notes.in.drive.forever@gmail.com>
2024-09-08 18:11:36 +0530

    Create Division of two numbers.asm

---
+++ Assembly Language Programming/Division of two numbers.asm
@@ -0,0 +1,19 @@
+section .data
+num1 db 5 ;
+num2 db 5
+
+section .bss
+res1 resb 1
+
+section .text
+global _start
+_start:
+
+mov al, [num1]
+mov bl, [num2]
+div bl
+mov [res1], al
+
+mov eax, 1
+xor ebx, ebx
+INT 80h
