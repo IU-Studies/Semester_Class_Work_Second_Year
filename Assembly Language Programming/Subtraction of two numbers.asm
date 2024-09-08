e1b52fb3a60cdc583ca5fc7fb2545dc85b58d570
IU-Studies <notes.in.drive.forever@gmail.com>
2024-09-08 18:11:00 +0530

    Create Subtraction of two numbers.asm

---
+++ Assembly Language Programming/Subtraction of two numbers.asm
@@ -0,0 +1,18 @@
+section .data
+num1 db 33h ;
+num2 db 32h
+
+section .bss
+res1 resb 1
+
+section .text
+global _start
+_start:
+
+mov al, [num1]
+sub al, [num2]
+mov [res1], al
+
+mov eax, 1
+mov ebx, 0
+INT 80h
