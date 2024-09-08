d4b16489f375bb5e287136f196cc56325b0ab848
IU-Studies <notes.in.drive.forever@gmail.com>
2024-09-08 18:12:05 +0530

    Create Multiplication of two numbers.asm

---
+++ Assembly Language Programming/Multiplication of two numbers.asm
@@ -0,0 +1,19 @@
+section .data
+num1 db 15 ;
+num2 db 3
+
+section .bss
+res1 resb 1
+
+section .text
+global _start
+_start:
+
+mov al, [num1]
+mov bl, [num2]
+mul bl
+mov [res1], al
+
+mov eax, 1
+xor ebx, ebx
+INT 80h
