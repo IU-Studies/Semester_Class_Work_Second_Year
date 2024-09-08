f04d40fc169d47430a2d12ac99f6e6643b4cf3f9
IU-Studies <notes.in.drive.forever@gmail.com>
2024-09-08 19:41:32 +0530

    Create Preorder Traversal on Binary Tree.c

---
+++ DSA/C/Preorder Traversal on Binary Tree.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <malloc.h>
+
+struct Node{
+  int val;
+  struct Node* left;
+  struct Node* right;
+};
+
+struct Node* CreateNode(int data){
+  struct Node* root;
+  root = (struct Node*) malloc(sizeof(struct Node));
+  root->val = data;
+  root->left = NULL;
+  root->right = NULL;
+  return root;
+}
+
+struct Node* BuildTree(){
+
+  int data;
+  printf("Enter data:- ");
+  scanf("%d",&data);
+
+  if (data == -1) return NULL;
+
+  struct Node* root = CreateNode(data); 
+
+  printf("Enter data in left of %d\n", data);
+  root->left = BuildTree();
+
+  printf("Enter data in right of %d\n", data);
+  root->right = BuildTree();
+
+  return root;
+}
+
+void Preorder(struct Node* root){
+  if (root == NULL) return;
+
+  printf("%d", root->val);
+  Preorder(root->left);
+  Preorder(root->right);
+}
+
+int main () {
+  struct Node* root = NULL;
+  root = BuildTree();
+  Preorder(root);
+}
