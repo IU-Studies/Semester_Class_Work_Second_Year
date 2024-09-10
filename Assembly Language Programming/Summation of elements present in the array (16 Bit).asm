b61c53f24b7ee950ccd71df91c5288edd61c5fd3
IU-Studies <notes.in.drive.forever@gmail.com>
2024-09-10 18:20:29 +0530

    Create Summation of elements present in the array (16 Bit).asm

---
+++ Assembly Language Programming/Summation of elements present in the array (16 Bit).asm
@@ -0,0 +1,27 @@
+section .data
+arr1 db 0ffh, 20h, 30h, 40h, 50h
+arrlen equ $-arr1
+
+section .bss
+sumlb resb 1
+sumhb resb 1
+
+section .text
+global _start
+_start:
+
+mov ebx, arr1
+mov cx, arrlen
+
+up1: mov al, [ebx]
+add [sumlb], al
+
+jnc down1
+inc byte [sumhb]
+
+down1: inc ebx
+loop up1
+
+mov eax, 1
+mov ebx, 0
+int 80h
