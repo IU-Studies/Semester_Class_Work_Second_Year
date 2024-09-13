29137342637fd725c91f1d34389ee7c0a15c2283
IU-Studies <notes.in.drive.forever@gmail.com>
2024-09-13 17:32:19 +0530

    Create Generators (Odd & Even number checker).py

---
+++ APP (Python) Classwork Examples/Generators (Odd & Even number checker).py
@@ -0,0 +1,11 @@
+def odd_even_checker(number_range):
+    for i in range(1, number_range):
+        if i % 2 == 0:
+            yield f"{i} is even"
+        else:
+            yield f"{i} is odd"
+
+data = odd_even_checker(20)
+
+for values in data:
+    print(values)
