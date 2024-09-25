61c65d565d1e09039004378cc678902aeec700fc
IU-Studies <notes.in.drive.forever@gmail.com>
2024-09-25 18:17:08 +0530

    Create Building a Binary Tree.py

---
+++ DSA/Python/Building a Binary Tree.py
@@ -0,0 +1,26 @@
+# In this program we will be creating a basic binary tree
+
+class Node:
+    def __init__(self, data):
+        self.val = data
+        self.left = None
+        self.right = None
+
+def build_tree():
+    data = int(input("Enter data: "))
+    
+    if data == -1:
+        return None
+    
+    root = Node(data)
+    
+    print(f"Enter data in left of {data}")
+    root.left = build_tree()
+    
+    print(f"Enter data in right of {data}")
+    root.right = build_tree()
+    
+    return root
+
+
+root = build_tree()
