e2fa574da623d84507c9c99881355a7830d40c85
IU-Studies <notes.in.drive.forever@gmail.com>
2024-09-29 17:09:04 +0530

    Create Building a Binary Tree.java

---
+++ DSA/Java/Building a Binary Tree.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class Node {
+    int val;
+    Node left, right;
+
+    Node(int data) {
+        val = data;
+        left = null;
+        right = null;
+    }
+}
+
+public class BinaryTree {
+    static Node buildTree() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter data (-1 to stop): ");
+        int data = scanner.nextInt();
+
+        if (data == -1) {
+            return null;
+        }
+
+        Node root = new Node(data);
+
+        System.out.println("Enter data in left of " + data);
+        root.left = buildTree();
+
+        System.out.println("Enter data in right of " + data);
+        root.right = buildTree();
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        Node root = buildTree();
+    }
+}
