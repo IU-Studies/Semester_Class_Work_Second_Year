8b41e6aaae40eeae8d992aebe2311ea9c776ff25
IU-Studies <notes.in.drive.forever@gmail.com>
2024-10-04 18:35:23 +0530

    Create Creating a Graph and Displaying.cpp

---
+++ DSA/C++/Creating a Graph and Displaying.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int n;
+  cout<<"Enter no. of vertices:- ";
+  cin>>n;
+  int graph[n][n];
+
+  for (int i=0; i<n; i++){
+    for (int j=0; j<n; j++){
+      cout<<"Point "<<static_cast<char>(65 + i)<<" to "<<static_cast<char>(65 + j)<<":-";
+      cin>>graph[i][j];
+    }
+  }
+  
+  for (int i=0; i<n; i++){
+    for (int j=0; j<n; j++){
+      cout<<graph[i][j]<<" ";
+    }
+    cout<<endl;
+}
+  
+}
