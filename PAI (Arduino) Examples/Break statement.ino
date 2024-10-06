a6f7bb27b06adcf64f570b4f8cedb1c494a21db7
IU-Studies <notes.in.drive.forever@gmail.com>
2024-10-06 17:28:41 +0530

    Create Break statement.ino

---
+++ PAI (Arduino) Examples/Break statement.ino
@@ -0,0 +1,17 @@
+const int ledPin = 13;
+
+void setup() {
+  pinMode(ledPin, OUTPUT); 
+
+  for (int i = 0; i < 10; i++) {
+    if (i == 5) {
+      break; 
+    }
+    digitalWrite(ledPin, HIGH);  
+    delay(500);                   
+    digitalWrite(ledPin, LOW);   
+    delay(500);                  
+  }
+}
+
+void loop() {}
