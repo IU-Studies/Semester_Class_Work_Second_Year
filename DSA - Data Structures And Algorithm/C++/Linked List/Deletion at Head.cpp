dadb962a90f8d05565be7966350c450d3d4249ca
IU-Studies <notes.in.drive.forever@gmail.com>
2024-10-12 08:14:54 +0530

    Create Deletion at Head.cpp

---
+++ DSA - Data Structures And Algorithm/C++/Linked List/Deletion at Head.cpp
@@ -0,0 +1,59 @@
+// In this program we will perform deletion at head of linked list
+
+#include <iostream>
+using namespace std;
+
+class Node{
+public:
+  int val;
+  Node* next;
+
+  Node (int data){
+    val = data;
+    next = NULL;
+  }
+};
+
+void insertAtTail(Node* &head, int val){
+  Node* new_node = new Node(val);
+  Node* temp = head;
+  while(temp->next != NULL){
+    temp = temp->next;
+  }
+  temp->next = new_node;
+}
+
+void insertAtHead(Node* &head, int val){
+  Node* new_node = new Node(val);
+  new_node->next = head;
+  head = new_node;
+}
+
+void deleteAtHead(Node* &head){
+  Node* temp = head;
+  head = head->next;
+  free(temp);
+}
+
+void display(Node* head){
+  Node* temp = head;
+  while (temp!=NULL){
+    cout<<temp->val<<"->";
+    temp = temp->next;
+  }
+  cout<<"NULL"<<endl;
+}
+
+
+int main(){
+
+  Node* head = NULL;
+  insertAtHead(head, 1);
+  display(head);
+  insertAtTail(head, 2);
+  display(head);
+  insertAtTail(head,3);
+  deleteAtHead(head);
+  display(head);
+
+}
