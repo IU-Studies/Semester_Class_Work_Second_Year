3ebe6891d3cde7ac422bc406f8153c8962f26c55
IU-Studies <notes.in.drive.forever@gmail.com>
2024-10-14 09:00:51 +0530

    Create Q3 Design a program to find.py

---
+++ APP - Bucket List Experiments/Experiment 5/Q3 Design a program to find.py
@@ -0,0 +1,23 @@
+""" Design a program to find the longest subsequence in a string that is also a palindrome. """
+
+def longest_palindromic_subsequence(s):
+    n = len(s)
+
+    dp = [[0] * n for _ in range(n)]
+
+    for i in range(n):
+        dp[i][i] = 1
+
+    for length in range(2, n + 1):
+        for i in range(n - length + 1):
+            j = i + length - 1
+            if s[i] == s[j]:
+                dp[i][j] = dp[i + 1][j - 1] + 2  
+            else:
+                dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])  
+
+    return dp[0][n - 1]
+
+s = "bbbab"
+result = longest_palindromic_subsequence(s)
+print("Length of the longest palindromic subsequence:", result)
