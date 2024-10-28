b3ebcb2c91fea5581af28e9364776955e5d93f34
IU-Studies <notes.in.drive.forever@gmail.com>
2024-10-28 11:37:41 +0530

    Create Write a Java program that checks whether two input strings are anagrams of each other.java

---
+++ JAVA - Bucket List Experiments/Question 2 B/Write a Java program that checks whether two input strings are anagrams of each other.java
@@ -0,0 +1,40 @@
+// Write a Java program that checks whether two input strings are anagrams of each other.
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnagramChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first string: ");
+        String firstString = scanner.nextLine();
+
+        System.out.print("Enter the second string: ");
+        String secondString = scanner.nextLine();
+
+        if (areAnagrams(firstString, secondString)) {
+            System.out.println("The strings are anagrams.");
+        } else {
+            System.out.println("The strings are not anagrams.");
+        }
+        scanner.close();
+    }
+
+    public static boolean areAnagrams(String str1, String str2) {
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+
+        if (str1.length() != str2.length()) {
+            return false; 
+        }
+
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        return Arrays.equals(charArray1, charArray2);
+    }
+}
