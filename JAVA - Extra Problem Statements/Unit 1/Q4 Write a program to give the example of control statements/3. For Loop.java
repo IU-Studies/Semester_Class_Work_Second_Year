e620ce3939e17f089b7693ca58cbd148311ed289
IU-Studies <notes.in.drive.forever@gmail.com>
2024-10-29 09:55:21 +0530

    Create 3. For Loop.java

---
+++ JAVA - Extra Problem Statements/Unit 1/Q4 Write a program to give the example of control statements/3. For Loop.java
@@ -0,0 +1,12 @@
+// Write a program to give the example of control statements.  
+// For Loop
+
+public class ForLoopExample {
+    public static void main(String[] args) {
+        System.out.println("First 10 natural numbers:");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
