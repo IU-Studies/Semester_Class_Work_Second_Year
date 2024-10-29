a67e5960100885373788973c64ca9fa16f694ee5
IU-Studies <notes.in.drive.forever@gmail.com>
2024-10-29 10:03:35 +0530

    Create 1. Prime number checking.java

---
+++ JAVA - Extra Problem Statements/Unit 1/Q6 Write a program to find the following/1. Prime number checking.java
@@ -0,0 +1,33 @@
+// Write a program to find the following
+// Prime number checking
+
+import java.util.Scanner;
+
+public class PrimeNumberCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number to check if it is prime: ");
+        int number = scanner.nextInt();
+
+        boolean isPrime = true;
+
+        if (number <= 1) {
+            isPrime = false;  
+        } else {
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;  
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+
+        scanner.close();
+    }
+}
