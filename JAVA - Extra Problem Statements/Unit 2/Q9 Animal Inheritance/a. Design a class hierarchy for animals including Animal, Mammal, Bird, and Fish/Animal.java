cf98926f3cf0c9a16d4034596c07c39c83baff00
IU-Studies <notes.in.drive.forever@gmail.com>
2024-10-29 10:24:37 +0530

    Create Animal.java

---
+++ JAVA - Extra Problem Statements/Unit 2/Q9 Animal Inheritance/a. Design a class hierarchy for animals including Animal, Mammal, Bird, and Fish/Animal.java
@@ -0,0 +1,25 @@
+public class Animal {
+    private String name;
+    private int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void eat() {
+        System.out.println(name + " is eating.");
+    }
+
+    public void sleep() {
+        System.out.println(name + " is sleeping.");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
