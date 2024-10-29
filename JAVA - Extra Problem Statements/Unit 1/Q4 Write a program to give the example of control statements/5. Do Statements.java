f74f4d98e1fc1328b0a347018c291323d3aec996
IU-Studies <notes.in.drive.forever@gmail.com>
2024-10-29 09:57:33 +0530

    Create 5. Do Statements.java

---
+++ JAVA - Extra Problem Statements/Unit 1/Q4 Write a program to give the example of control statements/5. Do Statements.java
@@ -0,0 +1,15 @@
+// Write a program to give the example of control statements.  
+// Do Statements
+
+public class DoWhileLoopExample {
+    public static void main(String[] args) {
+        int i = 1;
+
+        System.out.println("First 10 natural numbers:");
+
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 10);
+    }
+}
