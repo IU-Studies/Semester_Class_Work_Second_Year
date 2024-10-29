cf78ca8890321d80aaead457b730b3bd7587a381
IU-Studies <notes.in.drive.forever@gmail.com>
2024-10-29 09:58:43 +0530

    Create 1. Find the length of the array.java

---
+++ JAVA - Extra Problem Statements/Unit 1/Q5 Write a program to calculate the following/1. Find the length of the array.java
@@ -0,0 +1,13 @@
+// Write a program to calculate the following  
+// Find the length of the array.
+
+public class ArrayLengthExample {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};  
+
+        int length = numbers.length;  
+
+        System.out.println("The length of the array is: " + length);
+    }
+}
+
