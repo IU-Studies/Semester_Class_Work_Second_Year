90fdd7d0932373dd6bbbd2f6e7cf7c98ce233c20
IU-Studies <notes.in.drive.forever@gmail.com>
2024-10-29 09:56:45 +0530

    Create 4. While Statements.java

---
+++ JAVA - Extra Problem Statements/Unit 1/Q4 Write a program to give the example of control statements/4. While Statements.java
@@ -0,0 +1,15 @@
+// Write a program to give the example of control statements.  
+// While Statements
+
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        int i = 1;
+        
+        System.out.println("First 10 natural numbers:");
+
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
