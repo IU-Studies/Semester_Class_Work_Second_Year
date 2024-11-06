39d8cb1523128ed66eb7f10da0da7d24c0fd02e0
IU-Studies <notes.in.drive.forever@gmail.com>
2024-11-06 19:01:31 +0530

    Update Division of two numbers.asm

--- PAI - Assembly Language Programming/Division of two numbers.asm
+++ PAI - Assembly Language Programming/Division of two numbers.asm
@@ -1,19 +1,20 @@
 section .data
-num1 db 5 
-num2 db 5
+Dividend db 21       
+Divisor db 5       
 
 section .bss
-res1 resb 1
+quotient resb 1   
+remainder resb 1  
 
 section .text
 global _start
 _start:
+mov al, [Dividend]
+mov bl, [Divisor]
+div bl           
+mov [quotient], al
+mov [remainder], ah
 
-mov al, [num1]
-mov bl, [num2]
-div bl
-mov [res1], al
-
-mov eax, 1
-xor ebx, ebx
-INT 80h
+mov eax, 1       
+xor ebx, ebx     
+int 80h
