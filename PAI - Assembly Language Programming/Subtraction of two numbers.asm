b1f2a19bdfae84c50795631335fb7ecc712883b6
IU-Studies <notes.in.drive.forever@gmail.com>
2024-11-07 17:53:01 +0530

    Update Subtraction of two numbers.asm

--- PAI - Assembly Language Programming/Subtraction of two numbers.asm
+++ PAI - Assembly Language Programming/Subtraction of two numbers.asm
@@ -1,3 +1,6 @@
+;8086 ALP to perform Subtraction of 2, 8 Bit numbers stored in memory, store your result in memory. 
+;Use gdb debugger to execute and check the result. 
+
 section .data
 num1 db 33h 
 num2 db 32h
