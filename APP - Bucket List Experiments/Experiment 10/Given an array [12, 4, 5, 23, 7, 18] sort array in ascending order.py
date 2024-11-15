f44810646f5bd8b7fbdbb104e4f4797a3369cb41
IU-Studies <notes.in.drive.forever@gmail.com>
2024-11-15 16:18:08 +0530

    Create  Given an array [12, 4, 5, 23, 7, 18] sort array in ascending order.py

---
+++ APP - Bucket List Experiments/Experiment 10/Given an array [12, 4, 5, 23, 7, 18] sort array in ascending order.py
@@ -0,0 +1,8 @@
+#  Given an array [12, 4, 5, 23, 7, 18] sort array in ascending order
+
+import numpy as np
+
+array = np.array([12, 4, 5, 23, 7, 18])
+sorted_array = np.sort(array)
+
+print(sorted_array)
